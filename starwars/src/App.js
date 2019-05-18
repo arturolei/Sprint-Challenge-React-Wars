98daa5eb2f17c27987ae9618bdb536cfe563fb74
arturolei <artie.lei@gmail.com>
2019-05-18 10:30:19 -0700

    MVP almost complete. Fine-tuning and styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SWCharList from './components/SWCharList';
 import './App.css';
 
 class App extends Component {
@@ -33,6 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <SWCharList swCharData ={this.state.starwarsChars} />
       </div>
     );
   }
